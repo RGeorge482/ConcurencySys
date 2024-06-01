b0daf99784c864e157daef653b511d61e4fb548c
George <ratagheorghita@yahoo.com>
2024-06-02 00:31:16 +0100

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -23,3 +23,40 @@ socket.on("roomUsers", ({ room, users }) => {
   outputRoomName(room);
   outputUsers(users);
 });
+
+// Message from server
+socket.on("message", (message) => {
+  console.log(message);
+  outputMessage(message);
+
+  // Scroll down
+  gameMessages.scrollTop = gameMessages.scrollHeight;
+});
+
+// Output message to DOM
+function outputMessage(message) {
+  const div = document.createElement("div");
+  div.classList.add("message");
+  div.innerHTML = `<p class="meta">${message.username}<span>${message.time}</span></p>
+  <p class="text">
+  ${message.text}
+  </p>`;
+  document.querySelector(".game-messages").appendChild(div);
+}
+
+// Add room name to DOM
+function outputRoomName(room) {
+  roomName.innerText = room;
+}
+
+// Add users to DOM
+function outputUsers(users) {
+  userList.innerHTML = "";
+  users.forEach((user) => {
+    const li = document.createElement("li");
+    const won = user.won || 0;
+    const lost = user.lost || 0;
+    li.innerHTML = `${user.username} - Won: ${won} Lost: ${lost}`;
+    userList.appendChild(li);
+  });
+}
