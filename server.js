aa3b3adafdd249a1969dc55467459bde7fff3552
George <ratagheorghita@yahoo.com>
2024-06-02 00:25:18 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
 //server.js
+const path = require("path");
+const http = require("http");
+const express = require("express");
+const socketio = require("socket.io");
+const { Worker } = require("worker_threads");
+const formatMessage = require("./utils/messages");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+// Set static folder
+app.use(express.static(path.join(__dirname, "public")));
